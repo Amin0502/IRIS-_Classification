cebd5c45f5ea51b27dae82aabfa3539dc2a6382e
Amin0502 <108342961+Amin0502@users.noreply.github.com>
2022-06-30 21:08:23 +0800

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -38,7 +38,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.table(['Iris-Setosa', 'Iris-Versicolor', 'Iris-Virginica'])
+st.write(['Iris-Setosa', 'Iris-Versicolor', 'Iris-Virginica'])
 
 st.subheader('Prediction')
 # st.write(iris.target_names[prediction])
